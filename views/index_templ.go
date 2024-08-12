d08d8a96c4eff3ebe512088b870040f3e6552d43
Milan Petrovic <5rovicmilan8@gmail.com>
2024-08-12 22:55:30 +0200

    renames

--- views/index_templ.go
+++ views/index_templ.go
@@ -8,7 +8,7 @@ package views
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-import components "shorturl/views/comps"
+import "shorturl/views/components"
 
 func Index() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
